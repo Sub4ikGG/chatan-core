b76736f64b6429812f7e5c7105859aaaa45e3a7b
Sub4ik <zerotanker33@gmail.com>
2023-10-08 19:36:33 +0300

    - Убрал стандартные роуты

--- src/main/kotlin/ru/chatan/plugins/Databases.kt
+++ src/main/kotlin/ru/chatan/plugins/Databases.kt
@@ -51,34 +51,34 @@ fun Application.configureDatabases() {
 //            call.respond(HttpStatusCode.OK)
 //        }
         // Create user
-        post("/users") {
-            val user = call.receive<ExposedUser>()
-            val id = userService.create(user)
-            call.respond(HttpStatusCode.Created, id)
-        }
-        // Read user
-        get("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = userService.read(id)
-            if (user != null) {
-                call.respond(HttpStatusCode.OK, user)
-            } else {
-                call.respond(HttpStatusCode.NotFound)
-            }
-        }
-        // Update user
-        put("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = call.receive<ExposedUser>()
-            userService.update(id, user)
-            call.respond(HttpStatusCode.OK)
-        }
-        // Delete user
-        delete("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            userService.delete(id)
-            call.respond(HttpStatusCode.OK)
-        }
+//        post("/users") {
+//            val user = call.receive<ExposedUser>()
+//            val id = userService.create(user)
+//            call.respond(HttpStatusCode.Created, id)
+//        }
+//        // Read user
+//        get("/users/{id}") {
+//            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+//            val user = userService.read(id)
+//            if (user != null) {
+//                call.respond(HttpStatusCode.OK, user)
+//            } else {
+//                call.respond(HttpStatusCode.NotFound)
+//            }
+//        }
+//        // Update user
+//        put("/users/{id}") {
+//            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+//            val user = call.receive<ExposedUser>()
+//            userService.update(id, user)
+//            call.respond(HttpStatusCode.OK)
+//        }
+//        // Delete user
+//        delete("/users/{id}") {
+//            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+//            userService.delete(id)
+//            call.respond(HttpStatusCode.OK)
+//        }
     }
 }
 
